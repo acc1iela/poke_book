542fbcb40d4f5c6c01a685e8315b5eccbf739613
acc1iela <acc1iela@gmail.com>
2023-11-21 02:31:24 +0900

    update: pokemonの詳細な情報を取得してくる

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon/';
@@ -8,11 +8,20 @@ function App() {
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      loadPokemon(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = (data) => {
+    let _pokemonData = Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+  };
 
   return <div className="App">{loading ? <h1>Loading...</h1> : <h1>Data loaded!</h1>}</div>;
 }
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -3,3 +3,13 @@ export const getAllPokemon = (url) => {
     fetch(url).then((res) => res.json().then((data) => resolve(data)));
   });
 };
+
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+};
