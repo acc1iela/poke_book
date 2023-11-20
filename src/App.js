9d2b98170ec7a953c90aa4e0e6b6d853eba7dd05
acc1iela <acc1iela@gmail.com>
2023-11-21 02:21:32 +0900

    update: loading設定

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon/';
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return <div className="App">{loading ? <h1>Loading...</h1> : <h1>Data loaded!</h1>}</div>;
 }
 
 export default App;
