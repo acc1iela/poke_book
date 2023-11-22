eca5a76cc90faea0e367fad30097733b504a90b6
acc1iela <acc1iela@gmail.com>
2023-11-22 22:01:19 +0900

    update: 不要な引数を削除

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,11 +1,11 @@
 export const getAllPokemon = (url) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     fetch(url).then((res) => res.json().then((data) => resolve(data)));
   });
 };
 
 export const getPokemon = (url) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
