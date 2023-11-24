add689bec352cc43d573dc84309ad0cb0f5af104
acc1iela <acc1iela@gmail.com>
2023-11-24 18:20:57 +0900

    update: pokemon情報を取得

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,27 @@ const Card = ({ pokemon }) => {
       <div className="cardImg">
         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        {pokemon.types.map((type) => {
+          return (
+            <div className="cardType" key={type.type.name}>
+              {type.type.name}
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">Weight: {pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Height: {pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Ability: {pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
     </div>
   );
 };
